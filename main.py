0e02594e435a4ac884746c840da07124643df177
Miguel Jeronimo <mjeronimo@frd.org.mx>
2022-11-15 09:08:53 -0600

    Se creo la ruta para imprimir los catalogos en el API

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from app import app
+from coneccion import conn
+from flask import flash,request,jsonify 
+#from psycopg2.extras import Json
 
+@app.route('/api/catalogos')
+def catalogos():
+    try:
+        conexion = conn
+        cursorCatalogo = conexion.cursor()
+        sql = "select * from catalogo;"
+        ##conexion.commit()
+        cursorCatalogo.execute(sql)
+        rows = cursorCatalogo.fetchall()
+        return jsonify(rows)
+        #conexion.close()
+    except Exception as e:
+        print(e)
+    finally:
+        #cursor.close()
+        conexion.close()
+    
 
-@app.route('/api/saludo')
-def saludo():
-    return "hola todos"
 
 if __name__=="__main__":
     #app.run()
