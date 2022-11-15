4f70701d727cf2335106358a6bf9331632b80790
kennyquintal <97002418+kennyquintal@users.noreply.github.com>
2022-11-15 09:32:35 -0600

    Update coneccion.py

--- coneccion.py
+++ coneccion.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 conn = psycopg2.connect(    
-    host="10.10.58.75",
-    database="GESTION_DATOS",
-    user="mjeronimo",
-    password="Mexico123")
+    host="",
+    database="",
+    user="",
+    password="")
