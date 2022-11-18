24ff36ca035296a16e3f6a845c122b15491a860c
Miguel Jeronimo <mjeronimo@frd.org.mx>
2022-11-18 16:45:53 -0600

    Validar si el usuario existe o no existe

--- consultas/ObtenerClientes.py
+++ consultas/ObtenerClientes.py
@@ -10,6 +10,31 @@ def obtenerClientes():
         cursorClientes.execute(sql)
         rows = cursorClientes.fetchall()
         conn.commit()
+        if rows:
+            return 1
+        else:
+            return 0
+        #return rows
+    except Exception as e:
+        print("Error in transction Reverting all other operations of a transction ", e)
+        conn.rollback()
+        return "No tiene datos"
+    finally:
+        print("Hay conexion?",conn.closed)
+        if conn:
+            cursorClientes.close()
+            conn.close()
+            print(conn.closed)
+            print("conexion cerrada")
+
+def obtenerClienteCorreo(email):
+    conn = psycopg2.connect(host=host,database=database,user=user,password =password)
+    sql = f"""select cliente.email from cliente where email = '{email}'"""
+    cursorClientes = conn.cursor(cursor_factory=RealDictCursor)
+    try:
+        cursorClientes.execute(sql)
+        rows = cursorClientes.fetchall()
+        conn.commit()
         return rows
     except Exception as e:
         print("Error in transction Reverting all other operations of a transction ", e)
@@ -22,3 +47,4 @@ def obtenerClientes():
             conn.close()
             print(conn.closed)
             print("conexion cerrada")
+
