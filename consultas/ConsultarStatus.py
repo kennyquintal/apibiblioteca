a970f64be166c61011ceb4a6ae83d220dd98b3fc
Miguel Jeronimo <mjeronimo@frd.org.mx>
2022-11-17 18:00:36 -0600

    Consultar status

---
+++ consultas/ConsultarStatus.py
@@ -0,0 +1,24 @@
+from connection.coneccion import host,database,user,password
+from psycopg2.extras import RealDictCursor
+import psycopg2
+
+def obtenerStatus():
+    conn = psycopg2.connect(host=host,database=database,user=user,password =password)
+    sql = "select * from estatus;"
+    cursorStatus = conn.cursor(cursor_factory=RealDictCursor)
+    try:
+        cursorStatus.execute(sql)
+        rows = cursorStatus.fetchall()
+        conn.commit()
+        return rows
+    except Exception as e:
+        print("Error in transction Reverting all other operations of a transction ", e)
+        conn.rollback()
+        return "No tiene datos"
+    finally:
+        print("Hay conexion?",conn.closed)
+        if conn:
+            cursorStatus.close()
+            conn.close()
+            print(conn.closed)
+            print("conexion cerrada")
