865f25f12518d20f7da6e6e329975ccb349def15
Miguel Jeronimo <mjeronimo@frd.org.mx>
2022-11-18 18:03:46 -0600

    Validando si el libro existe o no existe

--- consultas/ObtenerLibros.py
+++ consultas/ObtenerLibros.py
@@ -21,4 +21,24 @@ def obtenerLibros():
             conn.close()
             print(conn.closed)
             print("conexion cerrada")
-            
+
+
+def obtenerLibroTitulo(nombreLibro):
+    conn = psycopg2.connect(host=host,database=database,user=user,password =password)
+    sql = f"""select l.titulo  from libro l where l.titulo = '{nombreLibro}'"""
+    try:
+        cursorCatalogo = conn.cursor(cursor_factory=RealDictCursor)
+        cursorCatalogo.execute(sql)
+        rows = cursorCatalogo.fetchall()
+        conn.commit()
+        return rows
+    except Exception as e:
+        print("Error in transction Reverting all other operations of a transction ", e)
+        conn.rollback()
+    finally:
+        print("Hay conexion?",conn.closed)
+        if conn:
+            cursorCatalogo.close()
+            conn.close()
+            print(conn.closed)
+            print("conexion cerrada")
